956d0c0240cf423a75f03a4ef9935d6d51f9af98
Agboola Sherriff <rylxes@gmail.com>
2025-02-26 20:08:15 +0100

    commit

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -18,10 +16,9 @@ function createWindow() {
     },
   });
 
-  // Load the index.html file.
-  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+  // Load A's built index.html
+  mainWindow.loadFile(path.join(__dirname, '../../build/index.html'));
 
-  // Open the DevTools in development.
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools();
   }
