98becda0e87063c591e3324763abdde71da38282
Agboola Sherriff <rylxes@gmail.com>
2025-02-28 01:18:56 +0100

    Updated release workflow to include the ui component
    Updated release workflow to include the ui component
    ---------
    
    Co-authored-by: Alexei Konovalov <alekseykonovalov@gmail.com>

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -18,14 +16,15 @@ function createWindow() {
     },
   });
 
-  // Load the index.html file.
-  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
 
-  // Open the DevTools in development.
+  // Load A's built index.html
+  mainWindow.loadFile(path.join(__dirname, '../../build/index.html'));
+
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools();
   }
 }
+
 
 app.whenReady().then(createWindow);
 
